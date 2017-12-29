977bf3ca7f819dbca711bc0c480f3c9af41fce6a
Xufeng Wei <xufeng.wei@ebaotech.com>
2017-12-29 22:59:45 +0800

    add the anted depandency && modify UserView

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import styles from './UserView.css';
+import PropTypes from 'prop-types';
+import {Table, Button, Popconfirm} from 'antd'; 
 
-function UserView() {
+
+function UserView({onDelete, users}) {
+  const columns = [{
+  	title: 'Name',
+  	dataIndex: 'name',
+  },{
+  	title: 'Age',
+  	dataIndex: 'age',
+  },{
+  	title: 'Actions',
+  	render:(text, record)=>{
+  		return (
+  			<Popconfirm title="Delete?" onConfirm={onDelete}>
+  				<Button>Delete</Button>
+  			</Popconfirm>
+  		)
+  	}
+  }];
+
+
   return (
-    <div className={styles.normal}>
-      Component: UserView
-    </div>
+    <Table dataSource={users} columns={columns} />
   );
 }
+
+UserView.propTypes = {
+
+};
 
 export default UserView;
