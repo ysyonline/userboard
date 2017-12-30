06e9f96ef48ffc6dfcf67b9fc7bc8755091efe1e
Xufeng Wei <xufeng.wei@ebaotech.com>
2017-12-30 13:47:49 +0800

    add post method for haeders

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,20 @@ import request from '../utils/request';
 
 const PAGE_SIZE = 5;
 
-export function query({ page }) {
-  return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
+
+
+export function query(condition) {
+
+  const settings = {
+    method: 'POST',
+    headers:{
+    	'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(condition)
+  };
+
+  const {page, pagesize = PAGE_SIZE} = condition;
+  const url = `/api/users?_page=${page}&_limit=${PAGE_SIZE}`;
+  
+  return request(url, settings);
 }
